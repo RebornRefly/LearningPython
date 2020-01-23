260c257a85bd312c339a1e1f89d88eae69f71190
RebornRefly <786746447@qq.com>
2020-01-23 16:29:31 +0800

    Question.

---
+++ Circle_1.py
@@ -0,0 +1,8 @@
+from math import pi
+
+# radius=23,Circumference=?,area=?
+r = 23
+c = 2 * pi * r
+area = pi * r * r
+print("Circumference = {0:.2f}".format(c))
+print("Area = {0:.2f}".format(area))
